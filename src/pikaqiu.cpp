b2319a50caaf5877368e824893ef31f7d8752281
ChrisKong <609027949@qq.com>
2019-10-08 13:49:41 +0800

    修复bug
    修复bug

--- src/pikaqiu.cpp
+++ src/pikaqiu.cpp
@@ -2,41 +2,47 @@
 #include "mtcnn.h"
 #include <time.h>
 
-int main()
-{
-    Mat image = imread("4.jpg");
-    mtcnn find(image.rows, image.cols);
-    clock_t start;
-    start = clock();
-    find.findFace(image);
-    imshow("result", image);
-    imwrite("result.jpg",image);
-    start = clock() -start;
-    cout<<"time is  "<<start/10e3<<endl;
-    // Mat image;
-    // VideoCapture cap(0);
-    // if(!cap.isOpened())  
-    //     cout<<"fail to open!"<<endl; 
-    // cap>>image;
-    // if(!image.data){
-    //     cout<<"读取视频失败"<<endl;  
-    //     return -1;
-    // }
+int main() {
+    int b = 1;
+    if (b == 0) {
+        Mat image = imread("4.jpg");
+        mtcnn find(image.rows, image.cols);
+        clock_t start;
+        start = clock();
+        find.findFace(image);
+        imshow("result", image);
+        imwrite("result.jpg", image);
+        start = clock() - start;
+//    cout<<"time is  "<<start/10e3<<endl;
+        cout << "time is " << (double) start / CLOCKS_PER_SEC * 1000 << "ms" << endl;
+        waitKey(0);
+        image.release();
+    } else {
+        Mat image;
+        VideoCapture cap(0);
+        if (!cap.isOpened())
+            cout << "fail to open!" << endl;
+        cap >> image;
+        if (!image.data) {
+            cout << "读取视频失败" << endl;
+            return -1;
+        }
 
-    // mtcnn find(image.rows, image.cols);
-    // clock_t start;
-    // int stop = 1200;
-    // while(stop--){
-    //     start = clock();
-    //     cap>>image;
-    //     find.findFace(image);
-    //     imshow("result", image);
-    //     if( waitKey(1)>=0 ) break;
-    //     start = clock() -start;
-    //     cout<<"time is  "<<start/10e3<<endl;
-    // }
-
-    waitKey(0);
-    image.release();
+        mtcnn find(image.rows, image.cols);
+        clock_t start;
+        int stop = 1200;
+//     while(stop--){
+        while (true) {
+            start = clock();
+            cap >> image;
+            find.findFace(image);
+            imshow("result", image);
+            if (waitKey(1) >= 0) break;
+            start = clock() - start;
+            cout << "time is " << (double) start / CLOCKS_PER_SEC * 1000 << "ms" << endl;
+        }
+        waitKey(0);
+        image.release();
+    }
     return 0;
 }
